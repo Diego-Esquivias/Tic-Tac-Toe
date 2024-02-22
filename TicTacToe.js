924ecaa3b060d6b73f5449138b1917d49782af60
Diego-Esquivias <141285541+Diego-Esquivias@users.noreply.github.com>
2024-02-22 16:54:02 -0700

    Last minute touches
    - Added comments to the js code file
    - Tweaked some code a bit

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,10 +1,11 @@
+// Creates variables that'll be used/updated throughout
 var playerTurn = 0
 var xWins = 0
 var oWins = 0
 
 var winningCombinations
 
-
+// Adds X or O to the button clicked depending on who's turn it is and makes that button unclickable to prevent errors
 const addText = (id) => {
     
     if ((playerTurn % 2) == 0) {
@@ -23,6 +24,7 @@ const addText = (id) => {
         document.getElementById("turns").innerHTML = "Turns: " + playerTurn
     }
 
+    // Creates a list of all the possible winning combinations that'll be used to see who wins
     winningCombinations = [
         ['1', '2', '3'],
         ['4', '5', '6'],
@@ -38,6 +40,7 @@ const addText = (id) => {
     setTimeout(winChecker, 100);
 }
 
+// Loops through the winningCombinations list and checks if each box is either all X or all O or a tie
 const winChecker = () => {
     let isWin = false
 
@@ -60,14 +63,17 @@ const winChecker = () => {
 
 }
 
+// When is winner is found it highlights the boxes that won
 const highlightWinningBoxes = (winningIndex) => {
     const [a, b, c] = winningCombinations[winningIndex];
-    document.getElementById(a).style.backgroundColor = "#AFE1AF";
-    document.getElementById(b).style.backgroundColor = "#AFE1AF";
-    document.getElementById(c).style.backgroundColor = "#AFE1AF";
+    document.getElementById(a).style.backgroundColor = "#e5e8c3";
+    document.getElementById(b).style.backgroundColor = "#e5e8c3";
+    document.getElementById(c).style.backgroundColor = "#e5e8c3";
 }
 
+// If Player X wins, tell the user to play again or cancel
 const xWon = () => {
+    // If user wants to play again, reset everything and update the winning score for X on the bottom of the screen
     if (confirm("Player X wins! Press ok to play again or cancel to stop")) {
         var elements = document.getElementsByClassName("btn")
         for (var i = 0; i < elements.length; i++) {
@@ -76,17 +82,20 @@ const xWon = () => {
             elements[i].style.backgroundColor = "#97988a"
             elements[i].disabled = false
         }
-        document.getElementsByClassName("turn").innerHTML = "Player: O"
+        document.getElementById("turn").innerHTML = "Player: X"
         xWins += 1
         document.getElementById("xWins").innerHTML = "X - " + xWins + " wins"
         playerTurn = 0
         document.getElementById("turns").innerHTML = "Turns: " + playerTurn
     } else {
+        // If they cancel, it reloads the whole page
         location.reload(true)
     }
 }
 
+// If Player O wins, tell the user to play again or cancel
 const oWon = () => {
+    // If user wants to play again, reset everything and update the winning score for O on the bottom of the screen
     if (confirm("Player O wins! Press ok to play again or cancel to stop")) {
         var elements = document.getElementsByClassName("btn")
         for (var i = 0; i < elements.length; i++) {
@@ -100,12 +109,15 @@ const oWon = () => {
         playerTurn = 0
         document.getElementById("turns").innerHTML = "Turns: " + playerTurn
     } else {
+        // If they cancel, it reloads the whole page
         location.reload(true)
     }
 }
 
+// If they tie, tell the user to play again or cancel
 const tie = () => {
     playerTurn = 0
+    // If user wants to play again, reset everything and doesn't update the scores on the bottom of the screen
     if (confirm("It's a tie! Press ok to play again or cancel to stop")) {
         var elements = document.getElementsByClassName("btn")
         for (var i = 0; i < elements.length; i++) {
@@ -116,6 +128,7 @@ const tie = () => {
         }
         document.getElementById("turns").innerHTML = "Turns: " + playerTurn
     } else {
+        // If they cancel, it reloads the whole page
         location.reload(true)
     }
 }
